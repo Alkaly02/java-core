2c78c33b259988eb7be2bf53ef57f8688147c096
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2024-08-02 23:01:21 +0000

    MultiThreading

---
+++ MultiThreading/src/models/Multi.java
@@ -0,0 +1,15 @@
+package models;
+
+public class Multi extends Thread {
+  @Override
+  public void run() {
+    for (int i = 0; i < 10; i++) {
+      System.out.println(i);
+      try {
+    	  Thread.sleep(1000);
+      }catch(InterruptedException e) {
+    	  e.printStackTrace();
+      }
+    }
+  }
+}
