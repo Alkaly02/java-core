8c98ab3821b6a25436ce5b41c11125d3741d31e2
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2024-08-05 21:47:18 +0000

    Thread synchronisation

---
+++ ThreadSync/src/main/MainActivity.java
@@ -0,0 +1,29 @@
+package main;
+
+import models.*;
+
+public class MainActivity {
+
+	public static void main(String[] args) {
+		Counter count1 = new Counter();
+		
+		CounterThread obj1 = new CounterThread(count1);
+		CounterThread obj2 = new CounterThread(count1);
+		
+		Thread t1 = new Thread(obj1);
+		Thread t2 = new Thread(obj2);
+		
+		t1.start();
+		t2.start();
+		
+		try {
+			t1.join();
+		}catch(InterruptedException ie) {
+			
+		}
+		
+		System.out.println("Counter thread : "+ count1.getCount());
+		
+	}
+
+}
