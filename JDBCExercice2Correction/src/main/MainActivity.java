4c5025e021287cd07d68b72559689c06d31cbab6
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2024-09-06 22:54:43 +0000

    feat: transactions and batch updates

--- JDBCExercice2Correction/src/main/MainActivity.java
+++ JDBCExercice2Correction/src/main/MainActivity.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 /*
 
@@ -11,7 +12,33 @@ You're welcome !!!!
 */
 
 class Student{
+	private String name;
+	private int age;
 	private Statement stmt;
+	private Connection con;
+	
+	public Student(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	private void setName(String name) {
+		this.name = name;
+	}
+	private String getName() {
+		return this.name;
+	}
+	
+	private int getAge() {
+		return this.age;
+	}
+	
+	private void setCon(Connection con) {
+		this.con = con;
+	}
+	private Connection getCon() {
+		return this.con;
+	}
 	
 	private void setStmt(Statement statement) {
 		this.stmt = statement;
@@ -25,6 +52,7 @@ class Student{
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			try {
 				Connection con = DriverManager.getConnection("jdbc:mysql://localhost/school", "root", "");
+				this.setCon(con);
 				this.setStmt(con.createStatement());
 				System.out.println("Connexion a la base de donnees reussie");
 			}catch(SQLException sqle) {
@@ -41,6 +69,52 @@ class Student{
 		String studentTableDDL = "CREATE TABLE IF NOT EXISTS students (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(100), age INT)";
 		this.getStmt().execute(studentTableDDL);
 		System.out.println("Creation de la table students reussie");
+	}
+//	create many students with transactions
+	public void saveManyStudents(ArrayList<String> queries) {
+		try {
+			this.getCon().setAutoCommit(false);
+			for(String query:queries) {
+				this.getStmt().executeUpdate(query);
+			}
+			this.getCon().commit();
+		}catch(SQLException ex) {
+			try {
+				this.getCon().rollback();
+			}catch(SQLException exc) {
+				System.out.println("Une erreur est survenue lors du rollback");
+				exc.printStackTrace();
+			}
+			System.out.println("Une erreur est survenue lors de l'insertion");
+			ex.printStackTrace();
+		}finally {
+			try {
+				this.getCon().setAutoCommit(true);
+			}catch(SQLException exe) {
+				exe.printStackTrace();
+			}
+		}
+	}
+//	create many students using batch
+	public void saveManyStudentsWithBatch(ArrayList<Student> students) {
+		try {
+			PreparedStatement stmt = this.getCon().prepareStatement("INSERT INTO students (name, age) VALUES (?,?)");
+			for(Student student:students) {
+				stmt.setString(1, student.getName());
+				stmt.setInt(2, student.getAge());
+				stmt.addBatch();
+			}
+			stmt.executeBatch();
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+		}
+		finally {
+			try {
+				this.getCon().setAutoCommit(true);
+			}catch(SQLException exe) {
+				exe.printStackTrace();
+			}
+		}
 	}
 //	create a new student
 	public void createStudent(String name, int age) throws SQLException {
@@ -81,7 +155,15 @@ public class MainActivity {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Student student = new Student();
+		ArrayList<String> queries = new ArrayList<String>();
+		queries.add("INSERT INTO students (name, age) VALUES ('Lamine MBODJ 3', 50) ");
+//		queries.add("INSERT INTO students (name, age) VAL ('Mamadou LAMINE', 50) ");
+		
+		ArrayList<Student> students = new ArrayList<Student>();
+		students.add(new Student("Moise", 23));
+		students.add(new Student("Lamine2", 34));
+
+		Student student = new Student("Alkaly", 200);
 		student.connectDb();
 		try {
 //			student.createStudentTable();
@@ -90,6 +172,8 @@ public class MainActivity {
 //			student.createStudent("Keba", 65);
 //			student.updateStudent(3, 80);
 //			student.deleteStudent(3);
+//			student.saveManyStudents(queries);
+			student.saveManyStudentsWithBatch(students);
 			student.getStudents();
 		}
 		catch(SQLException sqle) {
