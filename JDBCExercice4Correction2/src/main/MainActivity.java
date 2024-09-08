2cbd2db35af74d4b334afa1d42008b1efac17344
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2024-09-08 22:22:52 +0000

    improve code by using soc principle

--- JDBCExercice4Correction2/src/main/MainActivity.java
+++ JDBCExercice4Correction2/src/main/MainActivity.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.util.ArrayList;
+
+import models.Student;
 import models.StudentModel;
 
 /*
@@ -22,5 +25,33 @@ public class MainActivity {
 	public static void main(String[] args) {
 		StudentModel studentModel = new StudentModel();
 		studentModel.createStudentTable();
+		
+//		insertion
+		studentModel.insertStudent(new Student("Lka beast", 22));
+		
+//		Mise a jour
+		studentModel.updateStudent(2, new Student("Lka the performer", 23));
+
+//		suppression
+		studentModel.deleteStudent(3);
+
+//		Inserer plusieurs students en utilisant les transactions
+		ArrayList<String> studentQueries = new ArrayList<String>();
+		studentQueries.add("INSERT INTO students (name, age) VALUES ('Atos', 233)");
+		studentQueries.add("INSERT INTO students (name, age) VALUES ('Socium', 43)");
+		studentQueries.add("INSERT INTO students (name, age) VALUES ('Sonatel', 13)");
+		studentQueries.add("INSERT INTO students (name, age) VALUES ('Tonux mentor master', 53)");
+		studentQueries.add("INSERT INTO students (name, age) VALUES ('Medoune JavaMan', 32)");
+		studentQueries.add("INSERT INTO students (name, age) VALUES ('Optimus Debugger, the department', 15)");
+//		this should throw an rollback and the transaction will rollback
+		studentQueries.add("INSERT INTO students (name, age) VALES ('Mimistic', 40)");
+		studentModel.insertStudentsTransaction(studentQueries);
+
+//		Inserer plusieurs students en utilisant les batch
+		ArrayList<Student> students = new ArrayList<Student>();
+		students.add(new Student("Big company", 2025));
+		students.add(new Student("My goal", 2025));
+		studentModel.insertStudentsBatch(students);
+		
 	}
 }
--- JDBCExercice4Correction2/src/models/StudentModel.java
+++ JDBCExercice4Correction2/src/models/StudentModel.java
@@ -1,13 +1,16 @@
 package models;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class StudentModel {
 //	Creation de table student
 	DB db = new DB();
 	
 	public void createStudentTable() {
-		String studentDDL = "CREATE TABLE IF NOT EXISTS student (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(100), age INT)";
+		String studentDDL = "CREATE TABLE IF NOT EXISTS students (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(100), age INT)";
 		try {
 			db.getStmt().execute(studentDDL);
 			System.out.println("La table student a bien ete creee");
@@ -17,5 +20,97 @@ public class StudentModel {
 		}
 		
 	}
-	
+//	Inserer un etudiant
+	public void insertStudent(Student student) {
+		try {
+			PreparedStatement prepStmt = db.getCon().prepareStatement("INSERT INTO students (name, age) VALUES (?, ?)");
+			prepStmt.setString(1, student.getName());
+			prepStmt.setInt(2, student.getAge());
+			prepStmt.executeUpdate();
+			System.out.println("Ajout reussi");
+			prepStmt.close();
+		}catch(SQLException exc) {
+			exc.printStackTrace();
+			exc.getMessage();
+		}
+	}
+//	Mettre a jour un edudiant
+	public void updateStudent(int id, Student student) {
+		try {
+			PreparedStatement prepStmt = db.getCon().prepareStatement("UPDATE students SET name=?, age=? WHERE id=?");
+			prepStmt.setString(1, student.getName());
+			prepStmt.setInt(2, student.getAge());
+			prepStmt.setInt(3, id);
+			prepStmt.executeUpdate();
+			System.out.println("Mise a jour reussi");
+		}catch(SQLException exc) {
+			exc.getErrorCode();
+			exc.getMessage();
+			exc.printStackTrace();
+		}
+	}
+//	delete a student
+	public void deleteStudent(int id) {
+		try {
+			PreparedStatement prepStmt = db.getCon().prepareStatement("DELETE FROM students WHERE id=?");
+			prepStmt.setInt(1, id);
+			prepStmt.close();
+		}catch(SQLException exc) {
+			exc.getErrorCode();
+			exc.getMessage();
+			exc.getNextException();
+			exc.printStackTrace();
+		}
+	}
+//	Get all students
+	public ResultSet getStudents() throws SQLException {
+		String query = "SELECT * FROM students";
+		ResultSet rs = db.getStmt().executeQuery(query);
+		return rs;
+	}
+//	insert students using transactions
+	public void insertStudentsTransaction(ArrayList<String> queries) {
+			try {
+				db.getCon().setAutoCommit(false);
+				for(String query:queries) {
+					db.getStmt().executeUpdate(query);
+				}	
+				db.getCon().commit();
+			}catch(SQLException exc) {
+				try {
+					db.getCon().rollback();;
+				}catch(SQLException rollExc) {
+					rollExc.getMessage();
+					rollExc.printStackTrace();
+				}
+				exc.getErrorCode();
+				exc.getMessage();
+				exc.getNextException();
+				exc.printStackTrace();
+			}finally {
+				try {
+					db.getCon().setAutoCommit(true);
+				}catch(SQLException exc) {
+					exc.getMessage();
+					exc.printStackTrace();
+				}
+			}
+	}
+//	insert using batch
+	public void insertStudentsBatch(ArrayList<Student> students) {
+			try {
+				PreparedStatement prepStmt = db.getCon().prepareStatement("INSERT INTO students (name, age) VALUES (?, ?)");
+				for(Student student:students) {
+					prepStmt.setString(1, student.getName());
+					prepStmt.setInt(2, student.getAge());
+					prepStmt.addBatch();
+				}
+				prepStmt.executeBatch();
+				prepStmt.close();
+			}catch(SQLException exc) {
+				exc.getErrorCode();
+				exc.getMessage();
+				exc.printStackTrace();
+			}
+	}
 }
