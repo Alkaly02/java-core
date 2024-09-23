7b6689eddfc9c0fe8b8c5382b1ee117d5d50c2d9
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2024-09-23 21:16:39 +0000

    Transfert de fichier

--- TransfertFichierClient/src/main/Client.java
+++ TransfertFichierClient/src/main/Client.java
@@ -1,37 +1,39 @@
-package client;
+package main;
 
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.net.Socket;
 
-public class MyClient {
+public class Client {
 	private static DataOutputStream dataOutputStream;
 
 	public static void main(String[] args) throws Exception {
-		Socket socket = new Socket("localhost", 7777);
+		// TODO Auto-generated method stub
+		Socket socket = new Socket("localhost", 8888);
 		
 		dataOutputStream = new DataOutputStream(socket.getOutputStream());
 		
-		sendFile("alkaly.txt");
+		sendFile("message.txt");
 		
+		dataOutputStream.close();
 		socket.close();
-	
 	}
 	
 	private static void sendFile(String path) throws Exception {
 		File file = new File(path);
 		FileInputStream fis = new FileInputStream(file);
 		
-		int bytes = 0;
-		byte[] buffer = new byte[4 * 1024];
-		
 		dataOutputStream.writeLong(file.length());
 		
-		while((bytes = fis.read(buffer)) != -1) {
+		int bytes;
+		byte[] buffer = new byte[4*1024];
+		
+		while( (bytes = fis.read(buffer)) != -1) {
 			dataOutputStream.write(buffer, 0, bytes);
 			dataOutputStream.flush();
 		}
 		fis.close();
 	}
+
 }
