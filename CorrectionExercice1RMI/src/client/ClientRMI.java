be98bbed7b54406f03f05e7fcef1af6b6ca45513
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2024-09-29 23:12:42 +0000

    feat: allow the use to choose wich operation he wants to perform

--- CorrectionExercice1RMI/src/client/ClientRMI.java
+++ CorrectionExercice1RMI/src/client/ClientRMI.java
@@ -2,10 +2,18 @@ package client;
 
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Scanner;
 
 import interfaces.Calculatrice;
 
+
 public class ClientRMI {
+	enum ARITHMETIQUE_OPERATIONS{
+		addition,
+		soustraction,
+		multiplication,
+		division
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -13,10 +21,58 @@ public class ClientRMI {
 			Registry registryClient = LocateRegistry.getRegistry("localhost", 5555);
 			Calculatrice serviceCalculatrice = (Calculatrice)registryClient.lookup("CalculatriceService");
 			
-			 System.out.println("Addition 2+2: "+ serviceCalculatrice.addition(2, 2));
+			Scanner scn = new Scanner(System.in);	
+			System.out.print("Donner le premier nombre: ");
+			int a = scn.nextInt();
+			System.out.print("Donner le deuxieme nombre: ");
+			int b = scn.nextInt();
+			System.out.print("Quelle opeation souhaitez vous faire? :");
+			String userAnswer = scn.next();
+			ARITHMETIQUE_OPERATIONS desiredOperation = findByName(userAnswer);
+			
+			Boolean isOperationPossible = false;
+			
+			for(ARITHMETIQUE_OPERATIONS operation: ARITHMETIQUE_OPERATIONS.values()) {
+				if(operation.equals(desiredOperation)) {
+					isOperationPossible = true;
+				}
+			}
+			
+			if(!isOperationPossible) {
+				System.out.println("Veuillez choisir une operation arithmetique "+ ARITHMETIQUE_OPERATIONS.values());
+			}else {
+				String answer = new String();
+				answer = switch (desiredOperation) {
+					case addition ->{
+						yield "L'addition de " + a + "+"+ b +"="+serviceCalculatrice.addition(a, b);
+					}
+					case soustraction ->{
+						yield "La soustraction de "+a+"-"+b+"="+serviceCalculatrice.soustraction(a, b);
+					}
+					case multiplication ->{
+						yield "La multiplication de "+a+"*"+b+"="+serviceCalculatrice.multiplication(a, b);
+					}
+					case division ->{
+						yield "La division de "+a+"/"+b+"="+serviceCalculatrice.division(a, b);
+					}
+				};
+				System.out.println(answer);
+			}
+			scn.close();
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static ARITHMETIQUE_OPERATIONS findByName(String name) {
+		ARITHMETIQUE_OPERATIONS result = null;
+	    for (ARITHMETIQUE_OPERATIONS direction : ARITHMETIQUE_OPERATIONS.values()) {
+	        if (direction.name().equalsIgnoreCase(name)) {
+	            result = direction;
+	            break;
+	        }
+	    }
+	    return result;
 	}
 
 }
